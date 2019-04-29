c1a1397a483d9f3103556796374de78e719bc7e7
suman94sharma <suman448sharma@gmail.com>
2019-04-29 13:14:21 +0530

    First

---
+++ abDemoC.java
@@ -0,0 +1,31 @@
+package Abstract;
+
+public class abDemoC extends AbDemoP {
+	abDemoC(){
+		super(4);
+		System.out.println("default const of abstract child");
+	}
+
+		@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("abstract defined in child class");
+		
+	}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+		abDemoC ob=new abDemoC();
+		ob.show("suman");
+		ob.Adddetails(9834);
+		ob.display();
+		AbDemoP.BatchC(1234);
+		System.out.println(Companyname);
+		
+
+		}
+
+
+	
+
+}
